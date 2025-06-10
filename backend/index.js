df071bb92afbe2e47cc0f6e33287b3e9a4dae3af
Timothy Tran <thtran5@uci.edu>
2025-06-09 19:51:01 -0700

    production settings

--- backend/index.js
+++ backend/index.js
@@ -31,21 +31,23 @@ app.use(
         origin: allowedOrigins,
         credentials: true,
         methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-        allowedHeaders: ['Content-Type', 'Authorization']
+        allowedHeaders: ['Content-Type', 'Authorization'],
+        exposedHeaders: ['set-cookie']
     })
 );
 
 app.use(express.json());
-app.use(session({ // create cookies so user doesn't have to relog when refreshing page
-    secret: process.env.COOKIE_SECRET || "default_secret",
+app.use(session({
+    secret: process.env.COOKIE_SECRET,
     credentials: true,
     name: "sid",
     saveUninitialized: false,
     cookie: {
-        secure: process.env.ENVIRONMENT === "production" ? "true" : "auto",
+        secure: true,  // Force secure in production
         httpOnly: true,
+        sameSite: 'none', // Required for cross-site cookies
         expires: 1000 * 60 * 60 * 24 * 7, // 7 days
-        sameSite: process.env.ENVIRONMENT === "production" ? "none" : "lax",
+        domain: '.railway.app' // Adjust this to match your Railway domain
     },
 })
 );
