622543d5cabe2689ef14a2dae1a3d353b0d82794
Rafee <156030423+rafeer192@users.noreply.github.com>
2025-06-09 23:55:33 -0700

    fix'

--- backend/index.js
+++ backend/index.js
@@ -66,9 +66,8 @@ io.on("connect", socket => {
 });
 
 // EXPRESS SERVER
-server.listen(4000, () => {
-    console.log("Server is listening on port 4000");
-});
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 // health check
 app.get("/health", (req, res) => {
